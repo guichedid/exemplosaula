404261df80e138fb09e2ed9ce5b3d49b97a5c98d
guilherme chedid <guichedid2904@gmail.com>
2024-11-26 21:01:51 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,28 @@
+
+// Seleciona os elementos
+const caixa = document.getElementById("caixa");
+const botao = document.getElementById("botao");
+
+// Adiciona um evento de clique no botão
+botao.addEventListener("mouseout", () => {
+    // Verifica se a caixa tem a classe "visivel"
+    if (caixa.classList.contains("oculto")) {
+        // Troca a classe para "oculto"
+        caixa.classList.replace("oculto", "visivel");
+    } else {
+        // Troca a classe para "visivel"
+        caixa.classList.replace("visivel", "oculto");
+    }
+});
+botao.addEventListener("mouseover", () => {
+    // Verifica se a caixa tem a classe "visivel"
+    if (caixa.classList.contains("visivel")) {
+        // Troca a classe para "oculto"
+        caixa.classList.replace("visivel", "oculto");
+    } else {
+        // Troca a classe para "visivel"
+        caixa.classList.replace("oculto", "visivel");
+    }
+});
+
+    
